f1b32dde8caf061d6072454632bbd8dbd3bdd3d4
ilya bondarev <proghtmll@gmail.com>
2023-10-18 23:31:57 +0300

    second new dezine commit

---
+++ dist/images/slider..js
@@ -0,0 +1,25 @@
+let sliderContainer = document.querySelector('.slider-container');
+let sliderWrapper = document.querySelector('.slider-wrapper');
+let slider = document.querySelectorAll('.slider');
+if (window.innerWidth <= 1100) {
+  sliderContainer.className = 'swiper-container';
+  sliderWrapper.className = 'swiper-wrapper';
+  slider.forEach(item => item.className = 'swiper-slide');
+  new Swiper('.swiper-container', {
+    loop: true,
+    speed: 500,
+    autoplay: {
+      delay: 1700,
+      stopOnLastSlide: false,
+      disableOnInteraction: true
+    },
+    breakpoints: {
+      1100: {
+        slidesPerView: 5
+      },
+      460: {
+        slidesPerView: 2
+      }
+    }
+  });
+}
